0070a4f7fbe6d342cd88de349c286814a1a8e992
dante12588 <przemek.cwiertka@gmail.com>
2019-12-28 21:17:04 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends Component {
+  state = {
+    tasks: this.props.tasks,
+    draft: ''
+  }
+
+  updateDraft = event => {
+    this.setState({draft: event.target.value})
+  }
+
+  addTask = () => {
+    const {tasks, draft} = this.state;
+    const list = tasks;
+    list.push(draft);
+    this.setState({tasks: list, draft: ''})
+  }
+
+  render() {
+    const {title} = this.props;
+    const {tasks, draft} = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <ol>
+        {tasks.map( tasks => <li>{tasks}</li> )}
+        </ol>
+        <input type='text' onChange={this.updateDraft} value={draft} />
+        <button onClick={this.addTask}>Add task</button>
+      </div>
+    );
+  }
 }
+
+class App extends Component {
+  myTasks = [
+    'umyć się ',
+    'siedzieć na yt 10h',
+    'grać w lola'
+  ]
+  render(){
+    return(
+      <div>
+        <ToDoList title="Zadania Bartka na dziś " tasks = {this.myTasks} />
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
